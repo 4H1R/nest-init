be5a9c8504c03577af75fb27e6375bb892b1f021
4H1R <tmnt50792@gmail.com>
2022-08-08 13:11:14 +0430

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,21 @@ import { NestFactory } from '@nestjs/core';
 import { transformValidationErrors } from 'src/utils';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.setGlobalPrefix('/api');
+  app.enableCors();
+  app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       exceptionFactory: transformValidationErrors,
     }),
   );
+
   const config = new DocumentBuilder()
     .setTitle('Example Title')
     .setDescription('Example API Description')
